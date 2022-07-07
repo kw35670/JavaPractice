fb58c21fd9fc20f4c973a868c63123dfede1913f
riku <tkyu58690@gmail.com>
2022-07-07 11:44:00 +0900

    first commited

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+	public static void main(String[] args) {
+		System.out.println(35 - 10);
+		System.out.println(-5 * 2);
+		System.out.println(6 * 6 * 3.14);
+		System.out.println(31 + 31);
+		int x = 6;
+		System.out.println(x * x * 3.14);
+	}
+}
